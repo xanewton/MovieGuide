4da6665345c77d411e59bd2b1928cce0e369f6d7
Angel Newton <xanewton@gmail.com>
2017-02-08 10:59:56 -0600

    Sample info for people

--- app/src/main/java/com/xengar/android/movieguide/ui/PeopleFragment.java
+++ app/src/main/java/com/xengar/android/movieguide/ui/PeopleFragment.java
@@ -18,11 +18,35 @@ package com.xengar.android.movieguide.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.xengar.android.movieguide.R;
+import com.xengar.android.movieguide.adapters.PosterAdapter;
+import com.xengar.android.movieguide.data.ImageItem;
+import com.xengar.android.movieguide.data.Movie;
+import com.xengar.android.movieguide.data.MovieResults;
+import com.xengar.android.movieguide.service.DiscoverService;
+import com.xengar.android.movieguide.service.ServiceGenerator;
+import com.xengar.android.movieguide.sync.FetchItemListener;
+import com.xengar.android.movieguide.utils.CustomErrorView;
+import com.xengar.android.movieguide.utils.FragmentUtils;
+
+import java.util.List;
+
+import fr.castorflex.android.circularprogressbar.CircularProgressBar;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static android.content.res.Configuration.ORIENTATION_PORTRAIT;
+import static com.xengar.android.movieguide.utils.Constants.MOVIES;
+import static com.xengar.android.movieguide.utils.Constants.POSTER_SIZE_W342;
+import static com.xengar.android.movieguide.utils.Constants.TMDB_IMAGE_URL;
 
 
 /**
@@ -30,17 +54,187 @@ import com.xengar.android.movieguide.R;
  */
 public class PeopleFragment extends Fragment {
 
+    private static final String TAG = PeopleFragment.class.getSimpleName();
+
+    private CircularProgressBar progressBar;
+    private CustomErrorView mCustomErrorView;
+
+    private String mGenres;
+    private String mSortBy;
+    private String mMinRating;
+    private String mLang;
+
+    private RecyclerView recycler;
+    private PosterAdapter adapter;
+    private int mPage = 1;
+    private int mTotalPages = 1;
+
 
     public PeopleFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_people, container, false);
+        View view = inflater.inflate(R.layout.fragment_universal, container, false);
+        progressBar = (CircularProgressBar) view.findViewById(R.id.progressBar);
+        mCustomErrorView = (CustomErrorView) view.findViewById(R.id.error);
+
+        recycler = (RecyclerView) view.findViewById(R.id.recycler);
+        recycler.setHasFixedSize(true);
+
+        int columns = (getResources().getConfiguration().orientation == ORIENTATION_PORTRAIT) ? 3 : 4;
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), columns);
+        recycler.setLayoutManager(layoutManager);
+
+        mLang = FragmentUtils.getFormatLocale(getActivity());
+        mGenres = "";
+        mSortBy = null;
+        mMinRating = null;
+        adapter = new PosterAdapter(getContext(), MOVIES);
+        recycler.setAdapter(adapter);
+        FragmentUtils.updateProgressBar(progressBar, true);
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (!FragmentUtils.checkInternetConnection(getActivity())) {
+            Log.e(TAG, "Network is not available");
+            onLoadFailed(new Throwable(getString(R.string.network_not_available_message)));
+            return;
+        }
+
+        mTotalPages = 1;
+        LastItemListener listener = new LastItemListener();
+        recycler.addOnScrollListener(listener);
+        listener.loadPage();
+    }
+
+    private void onLoadFailed(Throwable t) {
+        mCustomErrorView.setError(t);
+        mCustomErrorView.setVisibility(View.VISIBLE);
+        FragmentUtils.updateProgressBar(progressBar, false);
+    }
+
+    /**
+     * Query the Movies page.
+     * @param listener
+     */
+    private void discoverMovies(final FetchItemListener listener){
+        DiscoverService service = ServiceGenerator.createService(DiscoverService.class);
+        Call<MovieResults> call =
+                service.discoverMovie(
+                        getString(R.string.THE_MOVIE_DB_API_TOKEN), mLang, mPage, mSortBy, mMinRating, mGenres);
+        call.enqueue(new Callback<MovieResults>() {
+            @Override
+            public void onResponse(Call<MovieResults> call, Response<MovieResults> response) {
+                if (response.isSuccessful()) {
+                    List<Movie> movies = response.body().getMovies();
+                    int adds = 0;
+                    for (Movie movie : movies) {
+                        adds += adapter.add(
+                                new ImageItem(TMDB_IMAGE_URL + POSTER_SIZE_W342 + movie.getPosterPath(),
+                                        Integer.parseInt(movie.getId()), movie.getTitle(), null));
+                    }
+                    if (adds != 0) {
+                        adapter.notifyDataSetChanged();
+                        FragmentUtils.updateProgressBar(progressBar, false);
+                        mTotalPages = response.body().getTotalPages();
+                        if (mPage < mTotalPages)
+                            mPage++;
+                        else
+                            listener.lastPageReached();
+                    }
+                    listener.onFetchCompleted();
+                } else {
+                    Log.i("TAG", "Res: " + response.code());
+                    listener.onFetchFailed();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MovieResults> call, Throwable t) {
+                onLoadFailed(t);
+            }
+        });
+    }
+
+
+    /**
+     * Listener to callback when the last item of the adpater is visible to the user.
+     * It should then be the time to load more items.
+     **/
+    private class LastItemListener extends RecyclerView.OnScrollListener implements FetchItemListener {
+
+        private boolean loadingState = false;
+        private boolean lastPageReached = false;
+
+        //Constructor
+        public LastItemListener(){
+        }
+
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+
+            // init
+            RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+            RecyclerView.Adapter adapter = recyclerView.getAdapter();
+
+            if (layoutManager.getChildCount() > 0) {
+                // Calculate
+                int indexOfLastItemViewVisible = layoutManager.getChildCount() -1;
+                View lastItemViewVisible = layoutManager.getChildAt(indexOfLastItemViewVisible);
+                int adapterPosition = layoutManager.getPosition(lastItemViewVisible);
+                boolean isLastItemVisible = (adapterPosition == adapter.getItemCount() -1);
+
+                /**
+                 * Here you should load more items because user is seeing the last item of the list.
+                 * Advice: you should add a bollean value to the class
+                 * so that the method {@link #loadPage()} will be triggered only once
+                 * and not every time the user touch the screen ;)
+                 **/
+                if (isLastItemVisible)
+                    loadPage();
+            }
+        }
+
+        /**
+         * Load page of posters.
+         */
+        public void loadPage() {
+            if (!loadingState && !lastPageReached) {
+                discoverMovies(this);
+                loadingState = true;
+            }
+        }
+
+        @Override
+        public void onFetchCompleted() {
+            loadingState = false;
+        }
+
+        @Override
+        public void onFetchFailed() {
+            loadingState = false;
+            onLoadFailed(new Throwable(getString(R.string.remote_service_connection_error)));
+        }
+
+        @Override
+        public void lastPageReached() {
+            lastPageReached = true;
+        }
     }
 
 }
